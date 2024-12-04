3f43d51e8c3566948368b4235c1b5d9666d37e64
hedwan02 <hedwan02@gmail.com>
2024-12-04 22:16:15 +0000

    commit

--- src/AuthenticatorWrapper.jsx
+++ src/AuthenticatorWrapper.jsx
@@ -1,42 +1,36 @@
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-import seniorImage from './assets/seniorsexcersing.png';
-import { Amplify } from 'aws-amplify';
-import awsmobile from './aws-exports';  // Import your AWS configuration
+import { Amplify } from "aws-amplify"
+import outputs from "../amplify_outputs.json"
 
-// Ensure AWS Amplify is configured
-Amplify.configure(awsmobile);
+Amplify.configure(outputs)
+
 
 export default function AuthenticatorWrapper({ children }) {
   return (
     <div>
-        <Authenticator socialProviders={['google']}>
-            {({ signOut, user }) => (
-                <div>
-                    {/* Show this header only when user is NOT authenticated */}
-                    {!user && (
-                      <header style={{ textAlign: "center", padding: "1rem" }}>
-                        <h1>Welcome to StayFIT! Let’s create a plan just for you.</h1>
-                      </header>
-                    )}
-
-                    {/* Show the child components (App) only when authenticated */}
-                    {user ? (
-                        <div>
-                            {children}
-                            <button onClick={signOut}>Sign Out</button>
-                        </div>
-                    ) : (
-                        <p>Loading...</p>
-                    )}
-                </div>
+      <Authenticator socialProviders={["google"]}>
+        {({ signOut, user }) => (
+          <div>
+            {/* Show this header only when user is NOT authenticated */}
+            {!user && (
+              <header style={{ textAlign: "center", padding: "1rem" }}>
+                <h1>Welcome to StayFIT! Let’s create a plan just for you.</h1>
+              </header>
             )}
-        </Authenticator>
 
-        {/* <img
-            src={seniorImage}
-            style={{ width: "100%", height: "auto", marginTop: "1rem" }}
-        /> */}
+            {/* Show the child components (App) only when authenticated */}
+            {user ? (
+              <div>
+                {children}
+                <button onClick={signOut}>Sign Out</button>
+              </div>
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div>
+        )}
+      </Authenticator>
     </div>
   );
 }
