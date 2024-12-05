bc38d83a2ff13624d9ece0945fc9130fb57fed92
hedwan02 <hedwan02@gmail.com>
2024-12-05 06:25:30 +0000

    commit

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,4 @@
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { a, defineData, type ClientSchema } from '@aws-amplify/backend';
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -6,9 +6,9 @@ adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any unauthenticated user can "create", "read", "update", 
 and "delete" any "Todo" records.
 =========================================================================*/
+
 const schema = a.schema({
-  userinfo: a
-    .model({
+  Todo: a.model({
       name: a.string(),
       gym_access: a.string(),
       conditions: a.string(),
@@ -18,16 +18,19 @@ const schema = a.schema({
       length_of_plan: a.string(),
       activity_types: a.string()
     })
-    .authorization((allow) => [allow.guest()]),
+    .authorization(allow => [allow.publicApiKey()])
 });
 
+// Used for code completion / highlighting when making requests from frontend
 export type Schema = ClientSchema<typeof schema>;
 
+// defines the data resource to be deployed
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'iam',
-  },
+    defaultAuthorizationMode: 'apiKey',
+    apiKeyAuthorizationMode: { expiresInDays: 30 }
+  }
 });
 
 /*== STEP 2 ===============================================================
